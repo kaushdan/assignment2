253cefb3374c83de758fa479ea334d162eb2b5e1
kaushdan <44858949+kaushdan@users.noreply.github.com>
2018-12-02 12:39:07 +0200

    Update TimeService.java

--- TimeService.java
+++ TimeService.java
@@ -1,6 +1,9 @@
 package bgu.spl.mics.application.services;
 
+import java.util.concurrent.TimeUnit;
+
 import bgu.spl.mics.MicroService;
+import bgu.spl.mics.application.messages.TickBroadcast;
 
 /**
  * TimeService is the global system timer There is only one instance of this micro-service.
@@ -14,14 +17,34 @@ import bgu.spl.mics.MicroService;
  */
 public class TimeService extends MicroService{
 
-	public TimeService() {
-		super("Change_This_Name");
-		// TODO Implement this
+	private int duration;
+	private int speed;
+	private int currentTime;
+	
+	public TimeService(String name,int speed,int duration) {
+		super(name);
+		this.duration=duration;
+		this.speed=speed;
+		this.currentTime=1;
 	}
 
 	@Override
 	protected void initialize() {
-		// TODO Implement this
+		sendBroadcast(new TickBroadcast(this.currentTime,false));
+		
+		subscribeBroadcast(TickBroadcast.class, message->{
+			try {
+				TimeUnit.MILLISECONDS.sleep(speed);
+			} catch (InterruptedException e) {}
+			currentTime++;
+			if(currentTime-1==duration) {
+				sendBroadcast(new TickBroadcast(this.currentTime,true));
+				terminate();
+			}
+			else {
+				sendBroadcast(new TickBroadcast(this.currentTime,false));
+			}
+		});
 		
 	}
 
