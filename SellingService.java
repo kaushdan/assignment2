fbdfd28c91a175f2440296e2d897092d6e6165cd
kaushdan <44858949+kaushdan@users.noreply.github.com>
2018-12-12 16:06:00 +0200

    Update SellingService.java

--- SellingService.java
+++ SellingService.java
@@ -1,6 +1,19 @@
 package bgu.spl.mics.application.services;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+import bgu.spl.mics.Future;
 import bgu.spl.mics.MicroService;
+import bgu.spl.mics.application.messages.BookOrderEvent;
+import bgu.spl.mics.application.messages.CheckAvailabiltyEvent;
+import bgu.spl.mics.application.messages.DeliveryEvent;
+import bgu.spl.mics.application.messages.TakeBookEvent;
+import bgu.spl.mics.application.messages.TerminateBroadcast;
+import bgu.spl.mics.application.messages.TickBroadcast;
+import bgu.spl.mics.application.passiveObjects.Customer;
+import bgu.spl.mics.application.passiveObjects.MoneyRegister;
+import bgu.spl.mics.application.passiveObjects.OrderReceipt;
+import bgu.spl.mics.application.passiveObjects.OrderResult;
 
 /**
  * Selling service in charge of taking orders from customers.
@@ -14,15 +27,51 @@ import bgu.spl.mics.MicroService;
  */
 public class SellingService extends MicroService{
 
-	public SellingService() {
-		super("Change_This_Name");
-		// TODO Implement this
+	private int currentTick;
+	private MoneyRegister moneyRegister;
+	private AtomicInteger count;
+	
+	public SellingService(String name, AtomicInteger count) {
+		super(name);
+		this.currentTick=1;
+		this.moneyRegister=MoneyRegister.getInstance();
+		this.count=count;
 	}
 
 	@Override
 	protected void initialize() {
-		// TODO Implement this
+		subscribeBroadcast(TerminateBroadcast.class, message->{
+			terminate();
+		});
 		
+		subscribeBroadcast(TickBroadcast.class, message->{
+			this.currentTick=message.getTick();
+		});
+		
+		subscribeEvent(BookOrderEvent.class, message->{
+			message.setProcessTick(this.currentTick);
+			Customer customer=message.getCustomer();
+			Future<Integer> bookFuture=(Future<Integer>)sendEvent(new CheckAvailabiltyEvent(message.getBookTitle()));
+			int price=bookFuture.get();
+			if(price!=-1) {
+				synchronized (customer) { 
+					if(customer.getAvailableCreditAmount()>=price) {
+						Future<OrderResult> resultFuture=(Future<OrderResult>) sendEvent(new TakeBookEvent(message.getBookTitle()));
+						OrderResult result=resultFuture.get();
+						if(result.equals(OrderResult.SUCCESSFULLY_TAKEN)) {
+							this.moneyRegister.chargeCreditCard(customer, price);
+							OrderReceipt receipt=new OrderReceipt(message.getOrderTick(), this.getName(), customer.getId(), message.getBookTitle(), price, this.currentTick,
+									message.getOrderTick(), message.getProcessTick());
+							customer.addReceipt(receipt);
+							this.moneyRegister.file(receipt);
+							sendEvent(new DeliveryEvent(customer.getDistance(), customer.getAddress()));
+						} 
+					}
+				}
+			}
+		});
+		
+		this.count.addAndGet(1);
 	}
 
 }
