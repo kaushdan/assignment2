b2adb3ddd2c6e7dc829f526548dc67eb4ed06717
kaushdan <44858949+kaushdan@users.noreply.github.com>
2018-12-12 15:52:17 +0200

    Update APIService.java

--- APIService.java
+++ APIService.java
@@ -1,6 +1,15 @@
 package bgu.spl.mics.application.services;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import bgu.spl.mics.MicroService;
+import bgu.spl.mics.application.messages.BookOrderEvent;
+import bgu.spl.mics.application.messages.TerminateBroadcast;
+import bgu.spl.mics.application.messages.TickBroadcast;
+import bgu.spl.mics.application.passiveObjects.Customer;
+import bgu.spl.mics.application.passiveObjects.Order;
 
 /**
  * APIService is in charge of the connection between a client and the store.
@@ -13,15 +22,44 @@ import bgu.spl.mics.MicroService;
  */
 public class APIService extends MicroService{
 
-	public APIService() {
-		super("Change_This_Name");
-		// TODO Implement this
+	private HashMap<Integer, LinkedList<String>> ordersByTick;
+	private Customer customer;
+	private AtomicInteger count;
+	
+	public APIService(String name,Customer customer, AtomicInteger count) {
+		super(name);
+		ordersByTick=new HashMap<>();
+		this.customer=customer;
+		Order[] orderSchedule=customer.getOrderSchedule();
+		for(Order order: orderSchedule) {
+			if(ordersByTick.containsKey(order.getTick())) {
+				this.ordersByTick.get(order.getTick()).add(order.getBookTitle());
+			}
+			else {
+				this.ordersByTick.put(order.getTick(), new LinkedList<String>());
+				this.ordersByTick.get(order.getTick()).add(order.getBookTitle());
+			}
+		}
+		this.count=count;
 	}
 
 	@Override
 	protected void initialize() {
-		// TODO Implement this
+		subscribeBroadcast(TerminateBroadcast.class, message->{
+			terminate();
+		});
 		
+		subscribeBroadcast(TickBroadcast.class, message->{
+			int tick=message.getTick();
+			if(this.ordersByTick.containsKey(tick)) {
+				LinkedList<String> orders=this.ordersByTick.get(tick);
+				for(String bookTitle: orders) {
+					sendEvent(new BookOrderEvent(this.customer,bookTitle,tick));
+				}
+			}
+		});
+		
+		this.count.addAndGet(1);
 	}
 
 }
