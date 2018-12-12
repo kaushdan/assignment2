39cddfd6d560cc96705f29b6fa7b899402e56421
kaushdan <44858949+kaushdan@users.noreply.github.com>
2018-12-12 16:05:18 +0200

    Update ResourcesHolder.java

--- ResourcesHolder.java
+++ ResourcesHolder.java
@@ -1,6 +1,7 @@
 package bgu.spl.mics.application.passiveObjects;
 
 import java.util.LinkedList;
+import java.util.Queue;
 
 import bgu.spl.mics.Future;
 
@@ -15,63 +16,132 @@ import bgu.spl.mics.Future;
  */
 public class ResourcesHolder {
 	
-	private static ResourcesHolder instance=null;
-	private LinkedList<DeliveryVehicle> freeVehicles=new LinkedList<>();
-	private LinkedList<DeliveryVehicle> occupiedVehicles=new LinkedList<>();
+	private DeliveryVehicle[] vehicles;
+	private Boolean[] acqired;
+	private Queue<Future<DeliveryVehicle>> futures;
+	private Object lockQ;
 	 
 	/**
      * Retrieves the single instance of this class.
      */
 	public static ResourcesHolder getInstance() {
-		if(instance==null) {
-			return new ResourcesHolder();
-		}
-		return instance;
+		  return SingletonHolder.instance;
 	}
 	
-	/**
+	private static class SingletonHolder {
+        private static ResourcesHolder instance = new ResourcesHolder();
+    }
+	
+	private ResourcesHolder() {
+		this.vehicles=null;
+		 this.futures=new LinkedList<>();
+		 this.acqired=null;
+		 this.lockQ=new Object();
+	}
+	
+	/** 
      * Tries to acquire a vehicle and gives a future object which will
      * resolve to a vehicle.
      * <p>
      * @return 	{@link Future<DeliveryVehicle>} object which will resolve to a 
      * 			{@link DeliveryVehicle} when completed.   
      */
-	public synchronized Future<DeliveryVehicle> acquireVehicle() {
-		while(freeVehicles.isEmpty()) {
-			try {
-				this.wait();
-			} catch (InterruptedException e) {}
-		}
-		DeliveryVehicle v=this.freeVehicles.remove();
-		this.occupiedVehicles.add(v);
-		Future<DeliveryVehicle> future=new Future<>();
-		future.resolve(v);
-		return future;
+	public Future<DeliveryVehicle> acquireVehicle() {
+		Future<DeliveryVehicle> future=new Future<>(); 
+		return findMin(future);
 	}
 	
+	private Future<DeliveryVehicle> findMin(Future<DeliveryVehicle> future) {
+		for(int i=0;i<this.acqired.length;i++)
+			synchronized (this.vehicles[i]) {
+				if(this.acqired[i]) {
+					this.acqired[i]=false;
+					future.resolve(this.vehicles[i]);
+					return future;
+				}
+			} 
+		this.futures.add(future);
+		return future;
+	}
+
 	/**
      * Releases a specified vehicle, opening it again for the possibility of
      * acquisition.
      * <p>
      * @param vehicle	{@link DeliveryVehicle} to be released.
      */
-	public synchronized void releaseVehicle(DeliveryVehicle vehicle) {
-		this.occupiedVehicles.remove(vehicle);
-		this.freeVehicles.add(vehicle);
-		this.notifyAll();
+	public void releaseVehicle(DeliveryVehicle vehicle) {
+		int index=find(vehicle);
+		synchronized (vehicle) {
+			this.acqired[index]=true;
+		}
+		Future<DeliveryVehicle> future;
+		synchronized (lockQ) {
+			future=this.futures.poll();
+		}
+		if(future!=null) {
+			setMin(future);
+		}
 	}
 	
+	private int find(DeliveryVehicle vehicle) {
+		int i=0;
+		for(;i<this.vehicles.length;i++) {
+			if(this.vehicles[i].equals(vehicle))
+				return i;
+		}
+		return i;
+	}
+
+	private void setMin(Future<DeliveryVehicle> future) {
+		boolean resolved=false;
+		for(int i=0;i<this.acqired.length;i++)
+			synchronized (this.vehicles[i]) {
+				if(this.acqired[i]) {
+					this.acqired[i]=false;
+					future.resolve(this.vehicles[i]);
+					resolved=true;
+				}
+			}
+		if(!resolved)
+			this.futures.add(future);
+	}
+
+	private void sort(DeliveryVehicle[] vehicles) {
+		for(int i=0;i<this.vehicles.length;i++) {
+			int min=findMin(i);
+			swap(i,min);
+		} 
+	}
+
+	private void swap(int i, int j) {
+		if(i!=j) {
+			DeliveryVehicle tmp=this.vehicles[j];
+			this.vehicles[j]=this.vehicles[i];
+			this.vehicles[i]=tmp;
+		}
+	}
+
+	private int findMin(int i) {
+		int min=i;
+		for(int j=i+1;j<this.vehicles.length;j++) {
+			if(this.vehicles[j].getSpeed()<this.vehicles[min].getSpeed())
+					min=j;
+		}
+		return min;
+	}
+
 	/**
      * Receives a collection of vehicles and stores them.
      * <p>
      * @param vehicles	Array of {@link DeliveryVehicle} instances to store.
      */
 	public void load(DeliveryVehicle[] vehicles) {
-		if(vehicles!=null) {
-			for(DeliveryVehicle v: vehicles) {
-				this.freeVehicles.add(v);
-			}
-		}
+		this.vehicles=vehicles;
+		sort(this.vehicles);
+		this.acqired=new Boolean[this.vehicles.length];
+		for(int i=0;i<this.vehicles.length;i++)
+			this.acqired[i]=true;
 	}
 
 }
