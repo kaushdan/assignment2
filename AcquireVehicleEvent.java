f9525c824f621f0fcc40af1a7863027dd1b0cf5b
kaushdan <44858949+kaushdan@users.noreply.github.com>
2018-12-12 15:53:51 +0200

    Update AcquireVehicleEvent.java

--- AcquireVehicleEvent.java
+++ AcquireVehicleEvent.java
@@ -4,21 +4,5 @@ import bgu.spl.mics.Event;
 import bgu.spl.mics.application.passiveObjects.DeliveryVehicle;
 
 public class AcquireVehicleEvent implements Event<DeliveryVehicle> {
-	
-	private int distance;
-	private String address;
-	
-	public AcquireVehicleEvent(int distance, String address) {
-		this.distance=distance;
-		this.address=address;
-	}
-	
-	public int getDistance() {
-		return this.distance;
-	}
-	
-	public String getAddress() {
-		return this.address;
-	}
  
 }
