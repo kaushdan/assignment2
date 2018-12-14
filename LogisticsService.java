390826b951b8d2334d68edef920046f3471ba3f1
kaushdan <44858949+kaushdan@users.noreply.github.com>
2018-12-14 20:15:40 +0200

    Update LogisticsService.java

--- LogisticsService.java
+++ LogisticsService.java
@@ -1,14 +1,16 @@
 package bgu.spl.mics.application.services;
 
+import java.util.LinkedList;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import bgu.spl.mics.Future;
 import bgu.spl.mics.MicroService;
 import bgu.spl.mics.application.messages.AcquireVehicleEvent;
 import bgu.spl.mics.application.messages.DeliveryEvent;
-import bgu.spl.mics.application.messages.ReleseVehicleEvent;
+import bgu.spl.mics.application.messages.ReleaseVehicleEvent;
 import bgu.spl.mics.application.messages.TerminateBroadcast;
 import bgu.spl.mics.application.passiveObjects.DeliveryVehicle;
+import bgu.spl.mics.application.passiveObjects.OrderReceipt;
 
 /**
  * Logistic service in charge of delivering books that have been purchased to customers.
@@ -31,16 +33,36 @@ public class LogisticsService extends MicroService {
 	@Override
 	protected void initialize() {
 		subscribeBroadcast(TerminateBroadcast.class, message->{
+//			System.out.println("received terminate broadcast "+getName());
 			terminate();
 		});
 		
 		subscribeEvent(DeliveryEvent.class, message->{
-			Future<DeliveryVehicle> future=sendEvent(new AcquireVehicleEvent());
-			if(future!=null) {
-				DeliveryVehicle v=future.get();
-				v.deliver(message.getAddress(), message.getDistance());
-				sendEvent(new ReleseVehicleEvent(v));
+//			System.out.println("sending AcquireVehicle Event "+getName());
+//			System.out.println("received future of future "+getName());
+			Future<Future<DeliveryVehicle>> futureOfFuture=sendEvent(new AcquireVehicleEvent());
+			if(futureOfFuture!=null) {
+//				System.out.println("Trying to get future of future data at: "+getName());
+				Future<DeliveryVehicle> future=futureOfFuture.get();
+				if(future!=null) {
+//					System.out.println("got future of future data at: "+getName());
+//					System.out.println("Trying to get future data at: "+getName());
+					DeliveryVehicle v=future.get();
+					if(v!=null) {
+//						System.out.println("got future data at: "+getName());
+						v.deliver(message.getAddress(), message.getDistance());
+//						System.out.println(v.toString()+" finished delivery "+getName());
+//						System.out.println("sending ReleaseVehicle Event "+getName()); 
+						sendEvent(new ReleaseVehicleEvent(v));
+					}
+//					else 
+//						System.out.println("vehicle is null");
+				}
+//				else
+//					System.out.println("future is null");
 			}
+//			else
+//				System.out.println("future of future is null");
 		});
 		
 		this.count.addAndGet(1);
